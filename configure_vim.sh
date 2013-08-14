f6c2fa88a17735bb3212bc1d62a6371ff66829e1
violetyk <yuhei.kagaya@nanapi.co.jp>
2013-08-14 20:29:51 +0900

    Add configure_vim

---
+++ configure_vim.sh
@@ -0,0 +1,22 @@
+./configure \
+--prefix=/usr/local/vim-7.4 \
+--with-compiledby="voletyk <yuhei.kagaya@gmail.com>" \
+--enable-fail-if-missing \
+--enable-fontset \
+--enable-pythoninterp \
+--with-python-config-dir=/usr/local/python-2.7.5/lib/python2.7/config \
+--enable-python3interp \
+--with-python3-config-dir=/usr/local/python-3.3/lib/python3.3/config-3.3m \
+--enable-perlinterp \
+--enable-rubyinterp \
+--enable-luainterp \
+--with-lua-prefix=/usr/local/lua-5.2.2 \
+--enable-cscope \
+--enable-multibyte \
+--with-x=no \
+--disable-gui \
+--disable-xim \
+--with-features=huge \
+--disable-selinux \
+--disable-gpm \
+--disable-darwin
