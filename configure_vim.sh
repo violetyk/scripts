18bc3f57971f94d7d274c7876b388ba497f7ed38
Yuhei Kagaya <yuhei.kagaya@gmail.com>
2013-11-20 19:23:37 +0900

    Update configure_vim.sh

--- configure_vim.sh
+++ configure_vim.sh
@@ -7,7 +7,6 @@
 --with-python-config-dir=/usr/local/python-2.7/lib/python2.7/config \
 --enable-python3interp \
 --with-python3-config-dir=/usr/local/python-3.3/lib/python3.3/config-3.3m \
---enable-perlinterp \
 --enable-luainterp \
 --with-lua-prefix=/usr/local/lua-5.2.2 \
 --enable-cscope \
